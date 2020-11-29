d8b39f1545155198a3b74864232113f1c6164763
dneves02 <71541305+dneves02@users.noreply.github.com>
2020-11-28 19:56:00 -0800

    ok

--- index.js
+++ index.js
@@ -6,5 +6,17 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', function(req, res) {
+    console.log('Hello Davi! Going to your home page');
+    var name = getUser();
+    var emailAddess = "dneves@zeeksgeeks.com";
+  
+    var params= {username: name, email: emailAddess};
+    res.render("home", params)
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+function getUser(){
+    return "Davi";
+}
