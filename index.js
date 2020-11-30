70a02d45a26e45245b57b431c99eb67d7a217ace
dneves02 <71541305+dneves02@users.noreply.github.com>
2020-11-29 23:32:10 -0800

    query db

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const { Pool } = require('pg');
+
+const connectionString = process.env.DATABASE_URL || 'postgres://dneves:mymenu123@localhost:5432/mymenuapp';
+const pool = new Pool({connectionString: connectionString});
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -14,10 +19,8 @@ express()
     res.header('Access-Control-Allow-Origin', '*');
     next();
   })
-  .get('/myrecipes', function(req, res) {
-    console.log('Hello, getting your recipes your from cookbook');
-    res.render("myrecipes")
-  })  
+  .get('/myrecipes', getRecipes)
+
   .get('/edamam', (req, res) => {
     request(
       { url: 'https://www.edamam.com/'},
@@ -32,3 +35,30 @@ express()
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
+function getRecipes(req, res) {
+    console.log('Hello, getting your recipes your from cookbook');
+    
+    var id = req.query.id;
+    console.log('Retriving recipe #' , id);
+    
+    getRecipeFromDb(id, function(error, result){
+      console.log('Back from the getRecipeFromDB with result: ', result);
+      res.json(result);
+    }) 
+  }
+
+function getRecipeFromDb(id, callback){
+
+  var sql = "SELECT * FROM recipes WHERE id = $1::int";
+  var params = [id];
+
+  pool.query(sql, params, function(err, result){
+    if(err) {
+      console.log("error with database: ", err)
+      callback(err, null)
+    }
+    console.log("Found DB result: " + JSON.stringify(result.rows))
+
+    callback(null, result.rows)
+  })
+}
