d22b165eb77b034e0904a3154c3914cf9b09d35d
dneves02 <71541305+dneves02@users.noreply.github.com>
2020-12-01 23:33:35 -0800

    ingredients

--- public/js/search.js
+++ public/js/search.js
@@ -1,36 +1,37 @@
 function search() {
-   // Get the value from the search box
-   var searchString = $('#txtSearch').val();
-   console.log('Searching for: ' + searchString);
+    // Get the value from the search box
+    var searchString = $('#txtSearch').val();
+    console.log('Searching for: ' + searchString);
  
-   // Set up the parameters to send to the API
-   var params = { q: searchString, app_id: '661c2e42', app_key: 'f2af3a7c771087f4a37bfadb93a34d97' };
+    // Set up the parameters to send to the API
+    var params = { q: searchString, app_id: '661c2e42', app_key: 'f2af3a7c771087f4a37bfadb93a34d97' };
  
    // Use jQuery to make the get request
-   $.get('https://api.edamam.com/search', params, function(data, status) {
-     // For debugging purposes, make a note that we're back
-     console.log('Back from server with the following results:');
-     console.log(status);
-     console.log(data);
+    $.get('https://api.edamam.com/search', params, function(data, status) {
+      // For debugging purposes, make a note that we're back
+      console.log('Back from server with the following results:');
+      console.log(status);
+      console.log(data);
  
-     updateResultList(data);
-   });
+      updateResultList(data);
+    });
  }
  
- function updateResultList(data) {
-   if (data.hits && data.hits.length > 0) {
-     var resultList = $('#ulResults');
-     resultList.empty();
+function updateResultList(data) {
+  if (data.hits && data.hits.length > 0) {
+    var resultList = $('#ulResults');
+    resultList.empty();
      
-     // you could use a forEach here as well...
-     for (var i = 0; i < data.hits.length; i++) {
-       var title = data.hits[i].recipe.label;
-       var url = data.hits[i].recipe.url;
-       var ingredients = data.hits[i].recipe.ingredients;
-
-       resultList.append('<li><p>' + title + '</p></li>');
-       resultList.append('<li><a title="Link" href='+ url +'>' + url + '</a></li>');
-       resultList.append('<li><p>' + ingredients + '</p></li>');
+    // you could use a forEach here as well...
+    for (var i = 0; i < data.hits.length; i++) {
+      var title = data.hits[i].recipe.label;
+      var url = data.hits[i].recipe.url;
+      resultList.append('<li><p>' + title + '</p></li>');
+      resultList.append('<li><a title="Link" href='+ url +'>' + url + '</a></li>');
+      for (var j = 0; j < data.hits[i].recipe.ingredients.length; j++) {
+        var ingredients = data.hits[i].recipe.ingredients[j];
+        resultList.append('<li><p>' + ingredients + '</p></li>');
+      }   
      }
    }
  }
