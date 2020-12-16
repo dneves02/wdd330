8ee744adb3fddeb6f847c7e89aab9246592c1f58
dneves02 <71541305+dneves02@users.noreply.github.com>
2020-12-16 00:11:35 -0800

    add new css styles

--- public/js/search.js
+++ public/js/search.js
@@ -27,76 +27,19 @@ function updateResultList(data) {
       var title = data.hits[i].recipe.label;
       var url = data.hits[i].recipe.url;
       var image = data.hits[i].recipe.image;
-      resultList.append('<li style="list-style-type: none;"><a href="'+ url +'"><img class="title_image" src="'+ image +'"><h3>' + title + '</h3></a>');
+      resultList.append('<li style="list-style-type: none;"><a href="'+ url +'"><img class="title_image" src="'+ image +'"><h1>' + title + '</h1></a>');
       
       //resultList.append('<h3><a href="'+ url +'">' + title + '</a></h3>');
       resultList.append('<ul>')
       for (var j = 0; j < data.hits[i].recipe.ingredients.length; j++) {
         var ingredient = data.hits[i].recipe.ingredients[j];
-        resultList.append('<li><p>' + ingredient.text + '</p></li>');
+        resultList.append('<li><p>' + ingredient.text + '</p></li><br /><br />');
         
       } 
       resultList.append('</ul></li>')
      }
    }
  }
-
-//styles and js animation 
-var words = document.getElementsByClassName('word');
-var wordArray = [];
-var currentWord = 0;
-
-words[currentWord].style.opacity = 1;
-for (var i = 0; i < words.length; i++) {
-  splitLetters(words[i]);
-}
-
-function changeWord() {
-  var cw = wordArray[currentWord];
-  var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
-  for (var i = 0; i < cw.length; i++) {
-    animateLetterOut(cw, i);
-  }
-  
-  for (var i = 0; i < nw.length; i++) {
-    nw[i].className = 'letter behind';
-    nw[0].parentElement.style.opacity = 1;
-    animateLetterIn(nw, i);
-  }
-  
-  currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
-}
-
-function animateLetterOut(cw, i) {
-  setTimeout(function() {
-		cw[i].className = 'letter out';
-  }, i*80);
-}
-
-function animateLetterIn(nw, i) {
-  setTimeout(function() {
-		nw[i].className = 'letter in';
-  }, 340+(i*80));
-}
-
-function splitLetters(word) {
-  var content = word.innerHTML;
-  word.innerHTML = '';
-  var letters = [];
-  for (var i = 0; i < content.length; i++) {
-    var letter = document.createElement('span');
-    letter.className = 'letter';
-    letter.innerHTML = content.charAt(i);
-    word.appendChild(letter);
-    letters.push(letter);
-  }
-  
-  wordArray.push(letters);
-}
-
-changeWord();
-setInterval(changeWord, 4000);
-
 
  //$('ul li:nth-child(1)').css('list-style-type','none');
 
