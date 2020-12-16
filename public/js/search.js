99d88e574a93d23fc313e1cdf448d1bb37d5d366
dneves02 <71541305+dneves02@users.noreply.github.com>
2020-12-15 23:31:42 -0800

    add new css styles

--- public/js/search.js
+++ public/js/search.js
@@ -22,7 +22,7 @@ function updateResultList(data) {
     var resultList = $('#ulResults');
     resultList.empty();
      
-    // you could use a forEach here as well...
+    // For loop to Iterate thru the results...
     for (var i = 0; i < data.hits.length; i++) {
       var title = data.hits[i].recipe.label;
       var url = data.hits[i].recipe.url;
@@ -40,6 +40,63 @@ function updateResultList(data) {
      }
    }
  }
+
+//styles and js animation 
+var words = document.getElementsByClassName('word');
+var wordArray = [];
+var currentWord = 0;
+
+words[currentWord].style.opacity = 1;
+for (var i = 0; i < words.length; i++) {
+  splitLetters(words[i]);
+}
+
+function changeWord() {
+  var cw = wordArray[currentWord];
+  var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
+  for (var i = 0; i < cw.length; i++) {
+    animateLetterOut(cw, i);
+  }
+  
+  for (var i = 0; i < nw.length; i++) {
+    nw[i].className = 'letter behind';
+    nw[0].parentElement.style.opacity = 1;
+    animateLetterIn(nw, i);
+  }
+  
+  currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
+}
+
+function animateLetterOut(cw, i) {
+  setTimeout(function() {
+		cw[i].className = 'letter out';
+  }, i*80);
+}
+
+function animateLetterIn(nw, i) {
+  setTimeout(function() {
+		nw[i].className = 'letter in';
+  }, 340+(i*80));
+}
+
+function splitLetters(word) {
+  var content = word.innerHTML;
+  word.innerHTML = '';
+  var letters = [];
+  for (var i = 0; i < content.length; i++) {
+    var letter = document.createElement('span');
+    letter.className = 'letter';
+    letter.innerHTML = content.charAt(i);
+    word.appendChild(letter);
+    letters.push(letter);
+  }
+  
+  wordArray.push(letters);
+}
+
+changeWord();
+setInterval(changeWord, 4000);
+
 
  //$('ul li:nth-child(1)').css('list-style-type','none');
 
