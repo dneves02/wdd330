91f7a09b6c52aef2bf974f585153125a96757adb
dneves <dneves02@gmail.com>
2021-09-20 12:32:38 -0700

    base.js updated

--- js/base.js
+++ js/base.js
@@ -3,4 +3,8 @@ const links = [
      label: "Week01",
      url: "week1/hello.html"
    }
- ]
+]
+function get_index(theButtonClicked){
+   document.getElementById("index").innerHTML = '<li><a href="./Week01/hello.html"><div class="button">Week 01<br/></div></a></li><li><a href="./Week02/week2.html"><div class="button">Week 02<br /></div></a></li>'
+        theButtonClicked.innerHTML = "My Assignments";
+}
