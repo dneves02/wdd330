6901943dff471f3846fc6b53e1d4572a684574c9
dneves <dneves02@gmail.com>
2021-09-20 13:55:48 -0700

    base.js updated

--- js/base.js
+++ js/base.js
@@ -1,18 +1,24 @@
-
+const links = [
+   {
+      label:"",
+      url:""
+   },
+   {
+     label: "Week01",
+     url: "./week01/hello.html"
+   },
+   {
+      label: "Week02",
+      url: "./week02/week2.html"
+   }
+]
 function get_index(theButtonClicked){
-   var links = [
-      {
-        label: "Week01",
-        url: "./week01/hello.html"
-      },
-      {
-         label: "Week02",
-         url: "./week02/week2.html"
-      }
-   ]
+
    var arrayLength = links.length;
-   for (var i in links){
+   document.getElementById("index").innerHTML = '<li><a href="./Week01/hello.html"><div class="button">Week 01<br/></div></a></li>
+   theButtonClicked.innerHTML = "My Assignments";
+   for (var i = 0; i < arrayLength; i++){
       document.getElementById("index").innerHTML = '<li><a href='+links[i].url+'><div class="button">'+links[i].label+'<br/></div></a></li>';
-      theButtonClicked.innerHTML = "My Assignments";
+
    }
 }
