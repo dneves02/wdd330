9271a98716f1385a1810a6714d450a9ee43f8de6
dneves <dneves02@gmail.com>
2021-11-18 15:38:52 -0800

    week10 update

--- week10/team/js/utilities.js
+++ week10/team/js/utilities.js
@@ -1,10 +1,11 @@
+const url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02'";
 export function getJSON(url) {
    return fetch(url)
      .then(function(response) {
        if (!response.ok) {
          throw Error(response.statusText);
        } else {
-         //console.log(response.json());
+         console.log(response.json());
          return response.json();
        }
      })
