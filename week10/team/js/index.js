56a863ee456f627b26db163e831bdebf76f5e0a9
dneves <dneves02@gmail.com>
2021-11-18 17:24:48 -0800

    week10 update

--- week10/team/js/index.js
+++ week10/team/js/index.js
@@ -1,4 +1,12 @@
-import QuakesController from './QuakesController.js';
 
-const myQuakesController = new QuakesController('#quakeList');
-myQuakesController.init();
+
+// const myQuakesController = new QuakesController('#quakeList');
+// myQuakesController.init();
+import QuakesController from './QuakesController.js';
+import buildNavigation from './routing.js';
+
+const navElement = document.getElementById('mainNav');
+buildNavigation(navElement);
+
+// const myQuakesController = new QuakesController('#quakeList');
+// myQuakesController.getQuakesByRadius();
--- week10/team/js/utilities.js
+++ week10/team/js/utilities.js
@@ -1,4 +1,3 @@
-const url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02'";
 export function getJSON(url) {
    return fetch(url)
      .then(function(response) {
