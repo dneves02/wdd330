60cb27965ecbe72b36f560c8ee396791d9edc978
dneves <dneves02@gmail.com>
2021-12-15 15:01:20 -0800

    week11 JSON server

---
+++ week10/week11.js
@@ -0,0 +1,18 @@
+import { makeRequest } from './authHelpers';
+
+makeRequest('login', 'POST', {
+   password: 'user1',
+   email: 'user1@email.com'
+   });
+
+
+document.querySelector(btn).eventListener('click'),login;
+
+
+
+
+
+import { Auth } from './auth';
+
+
+   
